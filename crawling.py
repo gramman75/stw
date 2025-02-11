599713f703b0ce2d7f329b01bd88bba97612d37a
gramman75 <gramman75@gmail.com>
2025-02-11 14:02:13 +0900

    final 1

--- crawling.py
+++ crawling.py
@@ -24,7 +24,7 @@ startStr = """
 <html lang="en">
 <head>
         <style>
-            .main table, th, td {
+            table, th, td {
             border: 1px solid black;
             border-collapse: collapse;
           }
@@ -41,15 +41,20 @@ lastSTr = """
 </body>
 </html>
 """
-cols = ['Tier','Type','Hull','Hull modifier','Shield modifier','Turn rate','Impulse modifier','Inertia rating','Warp core','Bonus Power','Bridge Officers','Weapons','Device slots','Consoles','Cost','Abilities','Admiralty stats','Released']
+cols = ['Name','Image','Tier','Type','Hull','Hull modifier','Shield modifier','Turn rate','Impulse modifier','Inertia rating','Warp core','Bonus Power','Bridge Officers','Weapons','Device slots','Consoles','Cost','Abilities','Admiralty stats','Image','Released']
 strList.append(startStr)
+strList.append('<tr>')
+for col in cols:    
+    strList.append('<th>'+col+'</th>') 
+strList.append('</tr>')    
+addBlank = False
 
 for idx, tr in enumerate(trs):
     if (idx < 2):
         continue
     
-    if (idx > 3):
-        break
+    # if (idx > 3):
+    #     break
     td = tr.find_all('td')[1]
     links = td.find_all('a')
     for lindex, link in enumerate(links):
@@ -72,27 +77,55 @@ for idx, tr in enumerate(trs):
 
                 for index, data in enumerate(datas):
                     cls = data.get('class')
-                    if cls != None and 'dataset' in cls: 
-                        label = data.find('div', class_='label').prettify().replace('src="/', 'src="https://stowiki.net/')
-                        entry = data.find('div', class_='entry').prettify().replace('src="/', 'src="https://stowiki.net/')
-                        
-                        for col in cols:
-                            pattern = "^"+col+"$"
-                            match = re.search(pattern, label)
-                            if match != None:
-                                print(f'=====> label : {label}, entry : {entry}')
-                                strList.append("<td><b>")
-                                strList.append(label)
-                                strList.append("</b></td>")
-                                strList.append("<td>")
-                                strList.append(entry)
-                                strList.append("</td>")
-                                break
+                    if cls != None:
+                        if 'dataset' in cls: 
+                            
+                            label = data.find('div', class_='label').prettify().replace('src="/', 'src="https://stowiki.net/')
+                            entry = data.find('div', class_='entry').prettify().replace('src="/', 'src="https://stowiki.net/')
+                            
+                            beforeCol = None
+                            for col in cols:
+                                pattern = col+":"
+                                # match = re.search(pattern, label)
+                                # if match != None:
+                                if pattern in label:                                
+                                    print(f'=====> label : {label}, entry : {entry}')
+                                    if col == 'Released' and addBlank == True:
+                                        strList.append("<td>empty</td>")
+                                    else:
+                                        addBlank = True 
+                                    # strList.append("<td><b>")
+                                    # strList.append(label)
+                                    # strList.append("</b></td>")
+                                    strList.append("<td>")
+                                    strList.append(entry)
+                                    strList.append("</td>")
+                                    beforeCol = col
+                                    break
+                        elif 'missionname' in cls :
+                            # strList.append("<td><b>")
+                            # strList.append("Name")
+                            # strList.append("</b></td>") 
+                            strList.append("<td>")
+                            content = data.prettify().replace('src="/', 'src="https://stowiki.net/')
+                            strList.append(content)
+                            strList.append("</td>")                                                    
                     else:
+                        addBlank = False
+                        url_3d = data.find('span', class_='viewer-3d-url-0')
+                        map_3d = data.find('span', class_='viewer-3d-map-0')
+                        if url_3d != None:
+                            url_3d.clear()
+
+                        if map_3d != None:
+                            map_3d.clear() 
+                    
+
                         strList.append("<td>")
                         content = data.prettify().replace('src="/', 'src="https://stowiki.net/')
                         strList.append(content)
-                        strList.append("</td>")
+                        strList.append("</td>") 
+
                 strList.append('</tr>')
         
 strList.append(startStr)
